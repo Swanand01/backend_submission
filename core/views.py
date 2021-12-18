2a21748536874def4bed8d30ce4baafef57761f6
Swanand01 <swanandmathekar@gmail.com>
2021-12-18 14:32:34 +0530

    Added sort by date to dashboard

--- core/views.py
+++ core/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from .models import Video
 
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .serializers import VideoSerializer
 from rest_framework.pagination import PageNumberPagination
-# Create your views here.
 
-# Change to JSONResponse later
+
 @api_view(["GET"])
 def video_endpoint(request):
     videos = Video.objects.all().order_by("-pub_date")
@@ -22,6 +20,9 @@ def video_endpoint(request):
 
 def dashboard(request):
     videos = Video.objects.all().order_by("-pub_date")
+    if request.GET.get("sort_by") == "oldest":
+        videos = Video.objects.all().order_by("pub_date")
+
     context = {
         "videos": videos
     }
