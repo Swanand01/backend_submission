24e6af62f83978757d324299d9f1c165b9832fca
Swanand01 <swanandmathekar@gmail.com>
2021-12-23 11:42:51 +0530

    Removing ignored files

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api/videos/", views.video_endpoint, name="video_endpoint"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-]
